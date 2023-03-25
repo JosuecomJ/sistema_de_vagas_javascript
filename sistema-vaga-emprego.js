e1f7cda76fa88b735c7deec2bda0b6ba73e4026d
Josué Nunes <josueemcontato@gmail.com>
2023-03-25 12:19:23 -0300

    Sistema de vagas JS

---
+++ sistema-vaga-emprego.js
@@ -0,0 +1,131 @@
+const vagas = []
+
+function listarVaga() {
+  const vagaEmTexto = vagas.reduce(function (textoFinal, vaga, indice) {
+    textoFinal += indice +". "
+    textoFinal += vaga.nome
+    textoFinal +="("+vaga.candidatos.length + "candidatos)\n"
+    return textoFinal
+  }, "") 
+  alert(vagaEmTexto) 
+}
+
+function novaVaga() {
+  const nome =prompt("informe no nome da vaga")
+  const descricao =prompt("Informe a descrição da vaga")
+  const dataLimite = prompt("Informe a data limite (dd/mm/aa)")
+
+  const confirmacao = confirm("Deseja criar uma nova vaga com essas informações?\n"+
+  "Nome: " + nome +
+  "\nDescrição: " + descricao+
+  "\nData Limite: " + dataLimite
+  )
+
+  if (confirmacao) {
+    const novaVaga = {nome,descricao,dataLimite, candidatos:[]}
+    vagas.push(novaVaga)
+    alert("Vaga Criada")
+  }
+}
+
+function exibirVaga() {
+  const indice = prompt("Informe o índice da vaga que deseja exibir:")
+  const vaga = vagas[indice]
+
+  if (indice >= vagas.length() || indice <0 ) {
+    alert("Indice invalido")
+    return
+  }
+
+  const candidatosEmTexto = vaga.candidatos.reduce((textoFinal, candidato) => textoFinal + "\n - " + candidato, "")
+
+  alert(
+    "Vaga nº " + indice +
+    "\nNome: " + vaga.nome +
+    "\nDescrição: " + vaga.descricao +
+    "\nData limite: " + vaga.dataLimite +
+    "\nQuantidade de candidatos: " + vaga.candidatos.length +
+    "\nCandidatos inscritos:" + candidatosEmTexto
+  )
+}
+function inscreverCandidato() {
+  const candidato = prompt("Informe o nome do(a) candidato(a):")
+  const indice = prompt("Informe o índice da vaga para a qual o(a) candidato(a) deseja se inscrever:")
+  const vaga = vagas[indice]
+
+  const confirmacao = confirm(
+    "Deseja inscrever o candidato " + candidato + " na vaga " + indice + "?\n" +
+    "Nome: " + vaga.nome + "\nDescrição: " + vaga.descricao + "\nData limite: " + vaga.dataLimite
+  )
+
+  if (confirmacao) {
+    vaga.candidatos.push(candidato)
+    alert("Inscrição realizada")
+  }
+}
+
+function excluirVaga() {
+  const indice = prompt("Informe o indice da vaga")
+  const vaga = vagas[indice]
+  const confirmacao = confirm("Deseja realmente excluir a vaga: " + indice +" Nome " + vaga.nome)
+
+  if (confirmacao) {
+    vaga.splice(indice, 1)
+    alert("Vaga excluida")
+  }
+}
+function exibirMenu() {
+  const opcao = prompt(
+    "Cadastro de Vagas de Emprego" +
+    "\n\nEscolha uma das opções abaixo: "+
+    "\n1) Listar Vagas"+
+    "\n2) Criar uma Nova Vaga"+
+    "\n3) Vizualizar uma Vaga"+
+    "\n4) Inscrever um Candidato"+
+    "\n5)  Excluir uma Vaga?"+
+    "\n6)  Sair"
+  )
+  return opcao
+}
+function executar() {
+  let opcao = ""
+
+  do {
+   opcao = exibirMenu()
+    
+    switch (opcao) {
+      case "1":
+        listarVaga()
+        
+        break;
+    
+      case "2":
+        novaVaga()
+        break;
+    
+      case "3":
+        exibirVaga()
+        break;
+    
+      case "4":
+        inscreverCandidato()
+        break;
+    
+      case "5":
+        inscreverCandidato()
+        
+        break;
+    
+      case "6":
+        alert("Saindo...")
+        break;
+    
+      default:
+        alert("Opção invalida")
+        break;
+    }
+    
+  } while (opcao !== 6);
+}
+
+executar()
